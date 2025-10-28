8df47aeeb624f13fb19ed96fd45f218ccb7b4edf
vaibhaviKharawane <kharawanevaibhavi@gmail.com>
2025-10-28 11:52:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + '/index.html');
+
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
